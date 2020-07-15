7cab24f132c7aaea2b647cb301e5458a4f75183c
estuprofe <luis@estuprofe.com>
2020-07-15 09:46:27 +0200

    Estructura de páginas hecha en el FRONTEND TODO: FRONTEND: incluir los campos de la base de datos. BACKEND: funciones de búsqueda y de exportar datos a EXCEL y PDF

--- FRONTEND.py
+++ FRONTEND.py
@@ -1,33 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Construido siguiendo el blog https://python-para-impacientes.blogspot.com/
+#módulos utilizados
+
+
+
 from BACKEND import *
-from tkinter import *
+import tkinter as tk                # python 3
+from tkinter import font as tkfont  # python 3
+# import Tkinter as tk     # python 2
+# import tkFont as tkfont  # python 2
+#setup
+nombreClinica='Elisa Isabel García López'
 
-class Aplicacion(Frame):
-	def prueba(self):
-		self.prueba["text"]="Funciona el botón"
-		print("funcionando")
 
-	def paginaClientes(self):
-		self.QUIT = Button(self)
-		self.QUIT["text"] = "Salir"
-		self.QUIT["fg"] = "red"
-		self.QUIT["command"] = self.QUIT
+class Aplicacion(tk.Tk):
 
-		self.QUIT.pack({"side": "left"})
+  def __init__(self, *args, **kwargs):
+    tk.Tk.__init__(self, *args, **kwargs)
 
-		self.prueba= Button(self)
-		self.prueba["text"] = "Botón de prueba"
-		self.prueba["command"] = self.prueba
+    self.fuente_titulo = tkfont.Font(
+        family='Helvetica', size=18, weight="bold", slant="italic")
+    self.geometry('800x600')
+      
+    self.title(f'Programa de facturación. Clinica {nombreClinica} ')
+    # the container is where we'll stack a bunch of frames
+    # on top of each other, then the one we want visible
+    # will be raised above the others
+    contenedor = tk.Frame(self)
+    contenedor.pack(side="top", fill="both", expand=True)
+    contenedor.grid_rowconfigure(0, weight=1)
+    contenedor.grid_columnconfigure(0, weight=1)
 
-		self.prueba.pack({"side" : "left"})
+    self.marcos = {}
+    for M in (PaginaInicial, PaginaCliente, PaginaFactura, PaginaResumen):
+      nombre_pagina = M.__name__
+      marco = M(padre=contenedor, controlador=self)
+      self.marcos[nombre_pagina] = marco
 
-	def __init__(self, master=None):
-		Frame.__init__(self, master)
-		self.pack()
-		self.paginaClientes()
+      # put all of the pages in the same location;
+      # the one on the top of the stacking order
+      # will be the one that is visible.
+      marco.grid(row=0, column=0, sticky="nsew")
 
-raiz = Tk()
-print("a ver que pasa")
-app=Aplicacion(master=raiz)
-app.mainloop()
-raiz.destroy()
+    self.mostrar_marco("PaginaInicial")
 
+  def mostrar_marco(self, nombre_pagina):
+    '''Show a frame for the given page name'''
+    marco = self.marcos[nombre_pagina]
+    marco.tkraise()
+
+
+class PaginaInicial(tk.Frame):
+
+  def __init__(self, padre, controlador):
+    tk.Frame.__init__(self, padre)
+    self.controlador = controlador
+    label = tk.Label(self, text=f"Programa de facturación. Clinica {nombreClinica}",
+                     font=controlador.fuente_titulo)
+    label.pack(side="top", fill="x", pady=10)
+
+    button1 = tk.Button(self, text="Clientes",
+                        command=lambda: controlador.mostrar_marco("PaginaCliente"))
+    button2 = tk.Button(self, text="Facturas",
+                        command=lambda: controlador.mostrar_marco("PaginaFactura"))
+    button3 = tk.Button(self, text="Resumen",
+                        command=lambda: controlador.mostrar_marco("PaginaResumen"))
+    button1.pack()
+    button2.pack()
+    button3.pack()
+
+class PaginaCliente(tk.Frame):
+
+  def __init__(self, padre,  controlador):
+    tk.Frame.__init__(self, padre)
+    self.controlador =  controlador
+    #título
+    label = tk.Label(self, text="Clientes", font=controlador.fuente_titulo)
+    label.grid(column=0, row=0)
+    #cuerpo
+
+
+
+    #pie
+    button = tk.Button(self, text="Ir al principio",
+                       command=lambda:  controlador.mostrar_marco("PaginaInicial"))
+    button.grid(column=0, row=1)
+
+
+class PaginaFactura(tk.Frame):
+
+  def __init__(self, padre,  controlador):
+    tk.Frame.__init__(self, padre)
+    self.controlador =  controlador
+    label = tk.Label(self, text="Facturas", font=controlador.fuente_titulo)
+    label.pack(side="top", fill="x", pady=10)
+    button = tk.Button(self, text="Ir al principio",
+                       command=lambda:  controlador.mostrar_marco("PaginaInicial"))
+    button.pack()
+
+class PaginaResumen(tk.Frame):
+
+  def __init__(self, padre,  controlador):
+    tk.Frame.__init__(self, padre)
+    self.controlador =  controlador
+    label = tk.Label(self, text="Resumen de Facturas", font=controlador.fuente_titulo)
+    label.pack(side="top", fill="x", pady=10)
+    button = tk.Button(self, text="Ir al principio",
+                       command=lambda:  controlador.mostrar_marco("PaginaInicial"))
+    button.pack()
+
+
+if __name__ == "__main__":
+  app = Aplicacion()
+  app.mainloop()
